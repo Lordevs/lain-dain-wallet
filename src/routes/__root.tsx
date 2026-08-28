a79d5671935aa52bf3a96ae5a3dcbaa6bd86eb79
Saadaan Hassan <webmaker9d@gmail.com>
2026-08-28 19:07:11 +0500

    refactor: remove Toaster component from RootComponent

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,6 @@ import AppShell from '@/components/layout/app-shell'
 import { useCapacitorSetup } from '@/hooks/use-capacitor-setup'
 import { usePushNotifications } from '@/features/notifications/hooks/use-push-notifications'
 import coinAnimation from '@/assets/coin.webp'
-import { Toaster } from '@/components/ui/sonner'
 
 function RootComponent() {
   const [showSplash, setShowSplash] = useState(true)
@@ -40,7 +39,6 @@ function RootComponent() {
       <AppShell>
         <Outlet />
       </AppShell>
-      <Toaster />
       {showSplash && (
         <div
           className="fixed inset-0 z-9999 bg-[#FEFAF1] flex flex-col items-center justify-center select-none"
